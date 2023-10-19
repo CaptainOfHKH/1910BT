0810a1d21a2ff457e14394420775ea6477e66f85
NTGiang2k2 <0306201019@caothang.edu.vn>
2023-10-19 16:10:34 +0700

    Giang up task2 Class

---
+++ lib/class/ListClass.dart
@@ -0,0 +1,12 @@
+// ignore: file_names
+class ListItem {
+  String imageUrl;
+  String title;
+  String summary;
+
+  ListItem({
+    required this.imageUrl,
+    required this.title,
+    required this.summary,
+  });
+}
